94f68d06c349b1f5401ecb7aac2010b98c38bab7
ChrisL <113984397+chrislou415@users.noreply.github.com>
2022-10-18 20:22:54 +1100

    first submmison

---
+++ vega_lite.js
@@ -0,0 +1,29 @@
+var vg_1 = "graph1.json"
+
+vegaEmbed('#first_chart', vg_1).then(function (result) {
+
+}).catch(console.error);
+
+var vg_2 = "graph2.json"
+
+vegaEmbed('#second_chart', vg_2).then(function (result) {
+
+}).catch(console.error);
+
+var vg_3 = "graph3.json"
+
+vegaEmbed('#third_chart', vg_3).then(function (result) {
+
+}).catch(console.error);
+
+var vg_4 = "graph4.json"
+
+vegaEmbed('#fourth_chart', vg_4).then(function (result) {
+
+}).catch(console.error);
+
+var vg_5 = "graph5.json"
+
+vegaEmbed('#fifth_chart', vg_5).then(function (result) {
+
+}).catch(console.error);
